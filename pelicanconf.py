0156f49c62fea7e51dabc99e4be926fe92ee0ade
jpneira <jpneira@riseup.net>
2015-05-14 02:09:48 -0300

    pelican

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+#==============================================================================
+# PELICAN SETTINGS FILE
+#==============================================================================
+
+PATH = "src"
+AUTHOR = "jpneira"
+SITENAME = "El blog de jp"
+THEME = "pelicanyan"
+CSS_FILE = "wide.css"
+FAVICON_IMG_URL = "favicon.ico"
+DISQUS_SITENAME = "jpneira.net"
+STATIC_PATHS = ['static']
+OUTPUT_PATH = ""
+
+SITEURL = 'http://jpneira.net'
+RELATIVE_URLS = True
+
+TIMEZONE = "America/Santiago"
+
+
+LOCALE = ""
+
+DEFAULT_DATE_FORMAT = ('%d %B %Y')
+LINKS = [
+    ('Acerca de mi', 'http://jpneira.net/pages/about.html'),
+    ('Code', 'https://github.com/jpneira'),
+    ('Messages courts', 'https://twitter.com/neirajp')
+]
+
+DISQUS_SITENAME = "jpneira.net"
---
+++ src/pages/about.rst
@@ -0,0 +1,11 @@
+Hola! soy juan pablo
+####################
+
+:slug: about
+
+Autodidacta // entusiasta del software y la cultura libre  // fil&#243;sofo part-time // sobreviviente del Y2K // un tanto-mucho chiflado...
+
+
+Don't hesitate to contact me at `alexis at notmyidea org`. The associated
+public key is `0x078805D867F56F12 <../jpneira.asc>`_.
+
